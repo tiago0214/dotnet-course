ccfd14158bc645f9f0f37c2dd656ba1d6927c29e
Tiago Souza <tiago0214@gmail.com>
2023-09-10 18:17:50 -0300

    att

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello Word!')
+
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then(function (response){
+        console.log(response)
+    })
+    .catch(function(error){
+        console.error(error)
+    })
+    .finally(function(){
+        console.log('Operação finalizada')
+    })
