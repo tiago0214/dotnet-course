12bdfc5481d471277fd99578f6b7f91df8bccb6c
Tiago Souza <tiago0214@gmail.com>
2023-09-15 22:55:25 -0300

    att

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,12 +1,27 @@
 
 
-const pokeApi = {
-    getPokemons : (offset = 0, limit = 10) => {
-        const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-        return fetch(url)
-                .then((response) => response.json())
-                .then((jsonBody) => jsonBody.results)
-                .catch((error) => console.error(error))
-    }
+const pokeApi = {}
+
+
+pokeApi.getPokemon = (pokemonAtual) => {
+
+    return fetch(pokemonAtual.url)
+            .then((convert) => convert.json())   
 
 }
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+            .then((response) => response.json())
+            .then((jsonBody) => jsonBody.results)
+            .then((pokemons) => pokemons.map(pokeApi.getPokemon))
+            .then((detailRequest) => Promise.all(detailRequest))
+}
+
+
+
+
+
+
