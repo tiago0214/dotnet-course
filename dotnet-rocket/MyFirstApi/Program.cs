40f1363cd2aea15349561919c12b9019af5fe1f4
Tiago Souza <tiago0214@gmail.com>
2024-11-13 19:03:18 -0300

    att

---
+++ dotnet-rocket/MyFirstApi/Program.cs
@@ -0,0 +1,29 @@
+using MyFirstApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var test = builder.Configuration.GetSection("Prop1").Value;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
