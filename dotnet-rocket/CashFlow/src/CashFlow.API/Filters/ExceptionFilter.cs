709c492a7f084c8998d653df111359419ed8e9de
Tiago Souza <tiago0214@gmail.com>
2024-11-25 01:58:44 -0300

    att

---
+++ dotnet-rocket/CashFlow/src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleProjectException(ExceptionContext context) { }
+
+    private void ThrowUnkownError(ExceptionContext context) { }
+}
--- dotnet-rocket/CashFlow/src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
+++ dotnet-rocket/CashFlow/src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6204a6c9bac8bffeab5c6832b8666ef52709c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c08c4efa886025dfa69491dfa1508924b352e32")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
