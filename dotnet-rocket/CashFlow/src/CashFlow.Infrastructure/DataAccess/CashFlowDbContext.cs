893d78a8b1568384d748a1821fe038425b4a4a2b
Tiago Souza <tiago0214@gmail.com>
2024-12-10 21:29:52 -0300

    att

--- dotnet-rocket/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ dotnet-rocket/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,15 +5,7 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Expense> Expenses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var conectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=root";
-
-        var version = new Version(8,0,40);
-        var serverVersion = new MySqlServerVersion(version);
-
-        optionsBuilder.UseMySql(conectionString, serverVersion);
-    }
 }
--- dotnet-rocket/CashFlow/src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ dotnet-rocket/CashFlow/src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,7 +1,9 @@
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace CashFlow.Infrastructure;
 
@@ -20,6 +22,11 @@ public static class DependencyInjectionExtension
 
     private static void AddDbContext(IServiceCollection services)
     {
-        services.AddDbContext<CashFlowDbContext>();
+        var conectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=root";
+
+        var version = new Version(8, 0, 40);
+        var serverVersion = new MySqlServerVersion(version);
+
+        services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(conectionString,serverVersion));
     }
 }
